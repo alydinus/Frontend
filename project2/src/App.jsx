6acc439f7d50ad975bbb398a3a7336fac01d5345
Alydin <alykulovalydin@gmail.com>
2024-11-21 22:06:44 +0600

    Button is now updating position of a person

--- project2/src/App.jsx
+++ project2/src/App.jsx
@@ -1,22 +1,24 @@
-import { useState } from 'react';
+import {useState} from 'react';
 import './App.css';
 
 const name = 'Алыдин';
-
+const positions = ['рядовой', 'сержант', 'лейтенант', 'капитан', 'полковник', 'генерал'];
 function App() {
+    const [index, setIndex] = useState(0);
     const [message, setMessage] = useState('');
-
+    const position = positions[index];
     const handleClick = () => {
-        setMessage('Привет, мир!');
+        setMessage('Должность повышена');
+        setIndex(index + 1);
     };
 
     return (
         <div className="person-info">
             <h2>Имя : {name}</h2>
-            <p>Должность: рядовой</p>
+            <p>Должность: {position}</p>
             <p>Компания: НКВД</p>
             <p>Возраст: 69</p>
-            <p>Пол: ламинат</p>
+            <p>Пол: Мужской</p>
             <p>Рост: 185</p>
             {message && <p>{message}</p>}
             <button onClick={handleClick}>Повысить в должности</button>
