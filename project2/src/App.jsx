9e6584f385ad9187b6cabf3bf3da11a1555318d5
Alydin <alykulovalydin@gmail.com>
2024-11-21 21:52:52 +0600

    Message is now appearing over button

--- project2/src/App.jsx
+++ project2/src/App.jsx
@@ -1,6 +1,15 @@
+import { useState } from 'react';
 import './App.css';
+
 const name = 'Алыдин';
+
 function App() {
+    const [message, setMessage] = useState('');
+
+    const handleClick = () => {
+        setMessage('Привет, мир!');
+    };
+
     return (
         <div className="person-info">
             <h2>Имя : {name}</h2>
@@ -9,11 +18,10 @@ function App() {
             <p>Возраст: 69</p>
             <p>Пол: ламинат</p>
             <p>Рост: 185</p>
-            <button onClick={"clicked"}>Кнопка</button>
+            {message && <p>{message}</p>}
+            <button onClick={handleClick}>Кнопка</button>
         </div>
     );
 }
-
-
 
 export default App;
