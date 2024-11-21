04d4370302258ace88985fb8ef73de53e64dd146
Alydin <alykulovalydin@gmail.com>
2024-11-21 21:38:10 +0600

    Component personInfo has been created.

--- project2/src/App.jsx
+++ project2/src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import './App.css';
+const name = 'Алыдин';
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <div className="person-info">
+            <h2>Имя : {name}</h2>
+            <p>Должность: рядовой</p>
+            <p>Компания: НКВД</p>
+            <p>Возраст: 69</p>
+            <p>Пол: ламинат</p>
+            <p>Рост: 185</p>
+        </div>
+    );
 }
 
-export default App
+
+
+export default App;
