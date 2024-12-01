3799dede3ff43aa9b3ca6a418befadd02840d8a6
Alydin <alykulovalydin@gmail.com>
2024-12-01 22:04:16 +0600

    Data initializated with useEffect hook

--- project3/src/App.js
+++ project3/src/App.js
@@ -1,16 +1,16 @@
-import './App.css';
-import {useState} from "react";
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  const [data, setData] = useState([]);
-  const [searchQuery, setSearchQuery] = useState('');
+    const [data, setData] = useState([]);
+    const [searchQuery, setSearchQuery] = useState('');
 
-  return (
-      <div>
-        <input type="text" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} />
-      </div>
-  );
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch('http://localhost');
+            const newData = await response.json();
+            setData(newData);
+        };
+        fetchData();
+    }, []);
 }
-
-export default App();
-
+export default App;
